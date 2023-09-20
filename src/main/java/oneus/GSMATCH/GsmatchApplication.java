b4cd8c3b3ff446abf7f6c5e48e457fa86b08ca83
신희성 <vvwv3092@gmail.com>
2023-09-20 20:42:50 +0900

    init

---
+++ src/main/java/oneus/GSMATCH/GsmatchApplication.java
@@ -0,0 +1,13 @@
+package oneus.GSMATCH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsmatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oneus/GSMATCH/GsmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package oneus.GSMATCH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
