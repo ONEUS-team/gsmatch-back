b65c77ce844ff0767a7a34ff0c790bc23490c7f5
taeyeon <tyeon42051@naver.com>
2023-10-10 09:53:46 +0900

    test

--- src/main/java/oneus/GSMATCH/GsmatchApplication.java
+++ src/main/java/oneus/GSMATCH/GsmatchApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class GsmatchApplication {
+	// 하이 나 태연
 
 	public static void main(String[] args) {
 		SpringApplication.run(GsmatchApplication.class, args);
