442b5bf3ddc46d0706d4a10468faf63f8cdd35a0
신희성 <vvwv3092@gmail.com>
2023-10-11 18:11:42 +0900

    Update Signup DTO

--- src/main/java/oneus/GSMATCH/domain/user/dto/request/SignupRequest.java
+++ src/main/java/oneus/GSMATCH/domain/user/dto/request/SignupRequest.java
@@ -1,8 +1,13 @@
 package oneus.GSMATCH.domain.user.dto.request;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.NotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -11,9 +16,48 @@ public class SignupRequest {
     private String username;
 
     @NotBlank
+    private String email;
+
+    @NotBlank
     private String password;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Grade grade;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private List<Major> major;
 
     @Getter
     private boolean admin = false;
     private String adminToken = "";
+
+
+    private enum Grade {
+        ONE,
+        TWO,
+        THREE;
+    }
+
+    private enum Gender {
+        MALE,
+        FEMALE;
+    }
+
+    private enum Major {
+        FRONT,
+        BACK,
+        DESIGN,
+        IOS,
+        AOS,
+        DEVOPS,
+        WORLD_SKILL,
+        IOT,
+        AI;
+    }
 }
