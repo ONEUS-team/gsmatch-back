001384e7e09b3d29722af73961bb3fddd7a087ac
신희성 <vvwv3092@gmail.com>
2023-10-12 15:18:25 +0900

    Update user dto validation

--- src/main/java/oneus/GSMATCH/domain/user/dto/request/LoginRequest.java
+++ src/main/java/oneus/GSMATCH/domain/user/dto/request/LoginRequest.java
@@ -1,11 +1,14 @@
 package oneus.GSMATCH.domain.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 }
