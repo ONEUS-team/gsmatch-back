6d601beda20b40d0f3ce2db165779c3c76d3cd99
신희성 <vvwv3092@gmail.com>
2023-12-13 20:50:43 +0900

    Add RoomEntity

---
+++ src/main/java/oneus/GSMATCH/domain/chat/entity/RoomEntity.java
@@ -0,0 +1,40 @@
+package oneus.GSMATCH.domain.chat.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import oneus.GSMATCH.domain.request.entity.RequestEntity;
+import oneus.GSMATCH.domain.user.entity.UserEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+@Builder
+public class RoomEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "room_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id")
+    private RequestEntity request;
+
+    @ManyToOne
+    @JoinColumn(name = "to_user")
+    private UserEntity toUser;
+
+    @ManyToOne
+    @JoinColumn(name = "from_user")
+    private UserEntity fromUser;
+
+    public static RoomEntity createRoom(RequestEntity request, UserEntity toUser, UserEntity fromUser) {
+        return RoomEntity.builder()
+                .request(request)
+                .toUser(toUser)
+                .fromUser(fromUser)
+                .build();
+    }
+
+}
